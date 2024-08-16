babed5fe5eed954744b06a673b63b59bd3945cd2
Kalle Minkner <info@kalleminkner.de>
2024-08-16 20:19:44 +0200

    add cod1 container

---
+++ cod1/bin/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd /home/container
+
+## with the help of https://github.com/pelican-eggs/yolks/blob/master/wine/entrypoint.sh (many thanks!)
+
+# info output
+echo "Running on:"
+echo "$(cat /etc/lsb-release)"
+echo "Current timezone: $(cat /etc/timezone)"
+wine --version
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# start xvfb
+if [[ $XVFB == 1 ]]; then
+    Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
+fi
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e "running: ${MODIFIED_STARTUP}"
+
+# Run the Server
+bash -c "${MODIFIED_STARTUP}"
