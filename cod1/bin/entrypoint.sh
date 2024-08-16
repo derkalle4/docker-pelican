e81f0557d82f976d9bb2a423c8015b7574160883
Kalle Minkner <info@kalleminkner.de>
2024-08-16 22:21:41 +0200

    draft: cod1 server

--- cod1/bin/entrypoint.sh
+++ cod1/bin/entrypoint.sh
@@ -13,11 +13,6 @@ wine --version
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
-# start xvfb
-if [[ $XVFB == 1 ]]; then
-    Xvfb :0 -screen 0 ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT}x${DISPLAY_DEPTH} &
-fi
-
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e "running: ${MODIFIED_STARTUP}"
